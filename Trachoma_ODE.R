aa869ce8446d47dae74860a797287b2498c5d6c6
Renata Retkute <r.retkute@yahoo.com>
2023-06-22 11:11:39 +0100

    Update and rename 02_COVID19.R to Trachoma_ODE.R

---
+++ Trachoma_ODE.R
@@ -0,0 +1,20 @@
+#' Trachoma model
+#'
+#' @param t 
+#' @param x 
+#' @param parameters 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+Trachoma_ODE_model <- function(t, x, parameters){ 
+  I <- x[1]
+  with( as.list(parameters), { 
+    dI <- beta * (N-I)*I/N - gamma*I
+    res <- c(dI)
+    list(res)
+  }
+  )
+}
